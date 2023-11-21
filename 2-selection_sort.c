754cc51ec09d7c816378edd63153896dfba67b2b
Juweria86 <jujumelmi143@gmail.com>
2023-11-21 06:43:07 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,24 +6,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min, temp;
+	size_t i, j;
+	int min, temp, flag;
 
-	if (!array)
+	if (array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
 		min = i;
+		flag = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 			{
 				min = j;
+				flag += 1;
 			}
 		}
 		temp = array[i];
 		array[i] = array[min];
 		array[min] = temp;
-		print_array(array, size);
+
+		if(flag != 0)
+			print_array(array, size);
 	}
 }
